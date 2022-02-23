d2bb29d5e0f3e2c3d15c1287fd58caa4b8187cd1
Jens Renken <renken@marum.de>
2022-02-23 16:47:49 +0100

    send also DBT record in minipos2depthprocessor

--- src/minipos2depthprocessor.cpp
+++ src/minipos2depthprocessor.cpp
@@ -6,12 +6,13 @@
  */
 
 #include "minipos2depthprocessor.h"
-
+#include <QDebug>
 MiniPos2DepthProcessor::MiniPos2DepthProcessor(const QString& parList)
     : DataProcessor(parList),
       mUseDepth(false)
 {
     mPKEL.setRecord(QByteArray("$PKEL,007,165809,LF,00.00,HF,02.55"));
+    mDBT.setRecord(QByteArray("$GPDBT,87.3,f,26.6,M,14.5,F"));
 }
 
 QList<QByteArray> MiniPos2DepthProcessor::processData(const QByteArray& data)
@@ -28,11 +29,19 @@ QList<QByteArray> MiniPos2DepthProcessor::processData(const QByteArray& data)
             if (mUseDepth) {
                 mPKEL[4] = n[6];
                 mPKEL[6] = n[6];
+                mDBT[3] = n[6];
+                mDBT.setField(1, n.toDouble(6) / 0.3048, 1);
+                mDBT.setField(5, n.toDouble(6) * 1.8288, 1);
             } else {
                 mPKEL[4] = n[7];
                 mPKEL[6] = n[7];
+                mDBT[3] = n[7];
+                mDBT.setField(1, n.toDouble(7) / 0.3048, 1);
+                mDBT.setField(5, n.toDouble(7) * 1.8288, 1);
             }
             out.append(mPKEL.sentence(true));
+            out.append(mDBT.sentence(true));
+            qDebug() << mDBT.sentence(true);
         } else
             continue;
     }
--- src/minipos2depthprocessor.h
+++ src/minipos2depthprocessor.h
@@ -24,6 +24,7 @@ public:
 
 private:
     NmeaRecord  mPKEL;
+    NmeaRecord  mDBT;
 
     bool        mUseDepth;
 };
